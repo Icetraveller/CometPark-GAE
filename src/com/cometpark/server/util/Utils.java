2569ef99a4a008aa61666e1f490cee7b5ffd0431
Elonix Thonarel <spider0quoll@gmail.com>
2014-04-16 18:33:31 -0500

    clean up code

--- src/com/cometpark/server/util/Utils.java
+++ src/com/cometpark/server/util/Utils.java
@@ -22,52 +22,88 @@ public class Utils {
 	public static final String SPOT_KIND = "Spot";
 	public static final String SPOT_ID = "Spot_id";
 	public static final String SPOT_AVAILABILITY = "availability";
-	
+
 	public static final String TOKEN_ID = "token_id";
-	
+
 	public static final int PERMIT_TYPE_GREEN = 1;
 	public static final int PERMIT_TYPE_GOLD = 2;
 	public static final int PERMIT_TYPE_PURPLE = 3;
 	public static final int PERMIT_TYPE_ORANGE = 4;
-	
+
 	public static final int STATUS_AVAILABLE = 0;
 	public static final int STATUS_OCCUPIED = 1;
-	
+
 	/**
 	 * Operation of update spot status only.
 	 */
 	public static final int TYPE_SPOTS_STATUS_UPDATE = 1;
-	
+
 	/**
 	 * Operation of update lot status only.
 	 */
 	public static final int TYPE_LOTS_STATUS_UPDATE = 2;
-	
+
 	/**
 	 * Operation of insert spots.
 	 */
 	public static final int TYPE_CREATE_SPOTS = 3;
-	
+
 	/**
 	 * Operation of insert lots.
 	 */
 	public static final int TYPE_CREATE_LOTS = 4;
+
+	/**
+	 * Operation of delete spot(s).
+	 */
 	public static final int TYPE_DELETE_SPOTS = 5;
+
+	/**
+	 * Operation of delete lot(s).
+	 */
 	public static final int TYPE_DELETE_LOTS = 6;
-	public static final int TYPE_REQUEST_SPOTS_IN_LOT = 7; 
-	public static final int TYPE_REQUEST_LOTS_INFO = 8; //for app to create
-	public static final int TYPE_REQUEST_SPOTS_INFO = 9; // for app to create
-	public static final int TYPE_REQUEST_LOTS_STATUS = 10; // exp 30/60
-	public static final int TYPE_SPOTS_INFO_UPDATE = 11; //change a lat of spot for example
-	public static final int TYPE_LOTS_INFO_UPDATE = 12; //change a lot name for example
+
+	/**
+	 * Operation of providing spots in a given lotId
+	 */
+	public static final int TYPE_REQUEST_SPOTS_IN_LOT = 7;
+
+	/**
+	 * Operation of providing latest lots info.
+	 */
+	public static final int TYPE_REQUEST_LOTS_INFO = 8;
+
+	/**
+	 * Operation of providing latest spots info.
+	 */
+	public static final int TYPE_REQUEST_SPOTS_INFO = 9;
+
+	/**
+	 * Operation of providing lots status. It includes reserved or not of
+	 * parking lots; Max spots numbers and how many spots are available in each
+	 * parking lot.
+	 */
+	public static final int TYPE_REQUEST_LOTS_STATUS = 10;
+
+	/**
+	 * Operation of change spots info data, for example changes of spot lat and
+	 * lng.
+	 */
+	public static final int TYPE_SPOTS_INFO_UPDATE = 11;
+
+	/**
+	 * Operation of change spots info data, for example changes of spot lat and
+	 * lng.
+	 */
+	public static final int TYPE_LOTS_INFO_UPDATE = 12; 
+	
 	public static final int _TYPE = 99;
-	
-	
+
 	public static final String JSON_TYPE = "type";
 	public static final String JSON_CONTROLLER_ID = "controllerId";
 	public static final String JSON_KEY_SPOTS = "spots";
 	public static final String JSON_KEY_LOTS = "lots";
-	
+
 	public static final String JSON_KEY_ID = "id";
 	public static final String JSON_KEY_LOT = "lot";
 	public static final String JSON_PERMIT_TYPE = "permit_type";
@@ -79,24 +115,10 @@ public class Utils {
 	public static final String JSON_KEY_LAT = "lat";
 	public static final String JSON_KEY_LNG = "lng";
 	public static final String JSON_KEY_DATA_VERSION = "dataVersion";
-	
-	
+
 	public static final String JSON_KEY_TOP_LEFT = "topLeft";
 	public static final String JSON_KEY_TOP_RIGHT = "topRight";
 	public static final String JSON_KEY_BOTTOM_LEFT = "bottomLeft";
 	public static final String JSON_KEY_BOTTOM_RIGHT = "bottomRight";
 
-	public static String initQuery(DatastoreService datastore) {
-		Query query = new Query(SPOT_KIND);
-		PreparedQuery pq = datastore.prepare(query);
-		HashMap<Long, Integer> map = new HashMap<Long, Integer>();
-		for (Entity result : pq.asIterable()) {
-			Key key = result.getKey();
-			Long id = key.getId();
-			String availability = result.getProperty(SPOT_AVAILABILITY).toString();
-			map.put(id, Integer.parseInt(availability));
-		}
-		JSONObject message = new JSONObject(map);
-		return message.toString();
-	}
 }
